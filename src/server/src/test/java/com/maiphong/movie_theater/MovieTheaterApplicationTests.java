1085a3b9ff1651ff96acad981ad18fd11b1e65a9
Tien Phong <tienphong2203@gmail.com>
2024-12-19 17:08:20 +0700

    Create base project

---
+++ src/server/src/test/java/com/maiphong/movie_theater/MovieTheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiphong.movie_theater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
