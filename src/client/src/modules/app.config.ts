0cafb2bcd783f64c93cea7166e7c9a0407b931a7
Tien Phong <tienphong2203@gmail.com>
2024-12-22 20:15:27 +0700

    FE: Config path and initialization provider http in app config

--- src/client/src/modules/app.config.ts
+++ src/client/src/modules/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { AUTH_SERVICE } from '../constants/injection.const';
+import { AuthService } from '../services/auth/auth-service.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  provideHttpClient(withFetch()),
+  {
+    provide: AUTH_SERVICE,
+    useClass: AuthService
+  }
+  ]
 };
--- src/client/src/modules/app.routes.ts
+++ src/client/src/modules/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
 import { CustomerLayoutComponent } from './layouts/customer-layout/customer-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { ManagerLayoutComponent } from './layouts/manager-layout/manager-layout.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: CustomerLayoutComponent,
         loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule)
+    },
+    {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    },
+    {
+        path: 'manager',
+        component: ManagerLayoutComponent,
+        loadChildren: () => import('./management/management.module').then(m => m.ManagementModule)
     },
     {
         path: '**',
